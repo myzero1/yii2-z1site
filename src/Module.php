8e2367fbbe7cebaa2c58124390b66c775591365a
myzero1 <myzero1@sina.com>
2018-11-03 17:41:03 +0800

    init module

---
+++ src/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace myzero1\z1site;
+
+/**
+ * z1site module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'backend\modules\z1site\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
