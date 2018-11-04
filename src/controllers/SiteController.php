21d0dba7d25e763573edae6bbdd095aa51ab12bc
myzero1 <myzero1@sina.com>
2018-11-04 19:42:35 +0800

    holdon

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -26,7 +26,7 @@ class SiteController extends Controller
                         'allow' => true,
                     ],
                     [
-                        'actions' => ['logout', 'index', 'main', 'notice'],
+                        'actions' => ['logout', 'index', 'main', 'notice', 'clear-assets-cache'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
