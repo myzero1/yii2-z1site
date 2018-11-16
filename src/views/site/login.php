11ce1f3a8ce05d0a688ac7cf5d33dbad87cbd9dc
myzero1 <myzero1@sina.com>
2018-11-16 10:12:43 +0800

    fix captcha

--- src/views/site/login.php
+++ src/views/site/login.php
@@ -125,6 +125,6 @@ JS;
 
 $this->registerJs($js, \yii\web\View::POS_HEAD);
 $this->registerJs($js2);
-$this->registerJs('$("#captchaimg").click()');
+$this->registerJs('$("#captchaimg").click()');// refresh captcha
 
 ?>
