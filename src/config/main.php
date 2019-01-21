b535a01390f2231d8adad9dfbfa0e8f7910fe186
myzero1 <myzero1@sina.com>
2019-01-21 16:43:58 +0800

    holdon

--- src/config/main.php
+++ src/config/main.php
@@ -6,16 +6,13 @@ return [
     'name' => 'yii2-z1site',
     'basePath' => myzero1\z1site\components\MainLoader::getAppPath(),
     'runtimePath' => myzero1\z1site\components\MainLoader::getAppPath() . '/runtime', //basePath,runtimePath,vendorPath,timeZone
-    // 'defaultRoute' => '/adminlteiframe/layout', // for adminlteiframe theme
     'controllerNamespace' => 'myzero1\z1site\controllers',
-    'controllerMap' => [
-        // 'adminlteiframe' => [ // for adminlteiframe theme
-        //     'class' => 'myzero1\adminlteiframe\controllers\SiteController'
-        // ],
-        'demo' => [ // for the menu of demo
-            'class' => 'myzero1\adminlteiframe\controllers\DemoController'
-        ]
-    ],
+    // 'defaultRoute' => '/adminlteiframe/layout', // for adminlteiframe theme
+    // 'controllerMap' => [
+    //     'adminlteiframe' => [ // for adminlteiframe theme
+    //         'class' => 'myzero1\adminlteiframe\controllers\SiteController'
+    //     ],
+    // ],
     'bootstrap' => [
         'classMap' => function(){
             // \Yii::$classMap['myzero1\z1site\controllers\ActController'] = '@vendor/myzero1/yii2-z1site/src/controllers/act/ActController.php';
@@ -25,56 +22,6 @@ return [
     'modules' => [
         'z1siteid' => [ // z1siteid mybe ajust
             'class' => 'myzero1\z1site\Module',
-            'params' => [
-                'menu' => [
-                    [
-                        'id' => "平台首页",
-                        'text' => "平台首页",
-                        'title'=>"平台首页",
-                        'icon' => "fa fa-dashboard",
-                        'targetType' => 'iframe-tab',
-                        'urlType' => 'abosulte',
-                        'url' => ['/z1siteid/site/index'],
-                        'isHome' => true,
-                    ],
-                    [
-                        'id' => "level1",
-                        'text' => "level1",
-                        'title'=>"level1",
-                        'icon' => "fa fa-dashboard",
-                        'targetType' => 'iframe-tab',
-                        'urlType' => 'abosulte',
-                        'url' => ['/z1siteid/site/level1'],
-                    ],
-                    [
-                        'id' => "level2",
-                        'text' => "level2",
-                        'title'=>"level2",
-                        'icon' => "fa fa-laptop",
-                        'url' => ['#'],
-                        'children' => [
-                            [
-                                'id' => "level21",
-                                'text' => "level21",
-                                'title'=>"level21",
-                                'icon' => "fa fa-angle-double-right",
-                                'targetType' => 'iframe-tab',
-                                'urlType' => 'abosulte',
-                                'url' => ['/z1siteid/site/level21'],
-                            ],
-                            [
-                                'id' => "level22",
-                                'text' => "level22",
-                                'title'=>"level22",
-                                'icon' => "fa fa-angle-double-right",
-                                'targetType' => 'iframe-tab',
-                                'urlType' => 'abosulte',
-                                'url' => ['/z1siteid/site/level22'],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
         ],
         'z1user' => [
             'class' => 'myzero1\z1user\Module',
@@ -84,6 +31,34 @@ return [
         ],
     ],
     'components' => [
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/myzero1/yii2-theme-adminlteiframe/src/views/adminlte', // using the adminlte theme
+                    // '@app/views' => '@vendor/myzero1/yii2-theme-adminlteiframe/src/views/adminlteiframe', // using the adminlteiframe theme
+                ],
+            ],
+        ],
+        'assetManager' => [
+            'class' => 'yii\web\AssetManager',
+            'forceCopy' => false,
+            /*'bundles'=> [
+                'myzero1\adminlteiframe\assets\php\components\LayoutAsset' => [
+                    'skin' => 'skin-red',// skin-{blue|black|purple|green|red|yellow}[-light],example skin-blue,skin-blue-light,
+                    'menuRefreshTab' => false, // true,false
+                    'jsVersion' => '1.7',
+                    'cssVersion' => '1.7',
+                ], // for adminlteiframe theme
+                'myzero1\adminlteiframe\assets\php\components\AdminLteAsset' => [
+                    'skin' => 'skin-red',// skin-{blue|black|purple|green|red|yellow}[-light],example skin-blue,skin-blue-light,
+                    'jsVersion' => '1.7',
+                    'cssVersion' => '1.7',
+                ], // for adminlte theme
+                'myzero1\adminlteiframe\assets\php\components\MainAsset' => [
+                    'showJParticle' => 'false', // 'false'/'true', default 'true',required
+                ], // for all theme
+            ],*/
+        ],
         'errorHandler' => [
             'errorAction' => 'z1siteid/site/error', // z1siteid mybe ajust
         ],
@@ -99,19 +74,6 @@ return [
             'identityClass' => 'myzero1\z1site\models\User',
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-        ],
-        'view' => [
-            'theme' => [
-                'pathMap' => [
-                    '@app/views' => '@vendor/myzero1/yii2-theme-adminlteiframe/src/views/adminlte', // using the adminlte theme
-                    // '@app/views' => '@vendor/myzero1/yii2-theme-adminlteiframe/src/views/adminlteiframe', // using the adminlteiframe theme
-                ],
-            ],
-        ],
-        'assetManager' => [
-            'class' => 'yii\web\AssetManager',
-            'forceCopy' => false,
-            // 'linkAssets' => true,//link to assets,no cache.used in develop.
         ],
         'request' => [
             'csrfParam' => '_csrf-backend',
